f212aa52c896d61c1b494f111327e17efce5376d
kency <marthashaka@gmail.com>
2018-01-09 15:08:02 +0300

    merging chnages

--- opensrp-drishti/src/main/java/org/ei/opensrp/drishti/LoginActivity.java
+++ opensrp-drishti/src/main/java/org/ei/opensrp/drishti/LoginActivity.java
@@ -141,9 +141,7 @@ public class LoginActivity extends AppCompatActivity {
         setDoneActionHandlerOnPasswordField();
         initializeProgressDialog();
 
-//        findViewById(R.id.credential_card).setBackground(new SmallDiagonalCutPathDrawable());
-//        ImageView v = (ImageView)findViewById(R.id.background);
-//        Glide.with(getApplicationContext()).load(R.drawable.clint_adair).into(v);
+
 
 
 
