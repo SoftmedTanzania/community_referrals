2d71b84061946c2035df7b609bd336467e27b61d
cozej4 <ilakozejumanne@gmail.com>
2018-01-29 22:38:36 +0300

    mionr bugs fixes

--- opensrp-app/src/main/java/org/ei/opensrp/domain/ReferralServiceDataModel.java
+++ opensrp-app/src/main/java/org/ei/opensrp/domain/ReferralServiceDataModel.java
@@ -11,24 +11,24 @@ import java.util.ArrayList;
 public class ReferralServiceDataModel {
 
     private static final String TAG = ReferralServiceDataModel.class.getSimpleName();
-    private String serviceName, id;
+    private String serviceName, serviceId;
 
     public ReferralServiceDataModel() {
 
     }
 
-    public ReferralServiceDataModel(String id, String name) {
-        this.id = id;
+    public ReferralServiceDataModel(String serviceId, String name) {
+        this.serviceId = serviceId;
         this.serviceName = name;
 
     }
 
     public String getId() {
-        return id;
+        return serviceId;
     }
 
-    public void setId(String id) {
-        this.id = id;
+    public void setId(String serviceId) {
+        this.serviceId = serviceId;
     }
 
     public String getName() {
@@ -39,23 +39,4 @@ public class ReferralServiceDataModel {
         this.serviceName = name;
     }
 
-    public static ArrayList<ReferralServiceDataModel> createReferralList() {
-        ArrayList<ReferralServiceDataModel> visitedMom = new ArrayList<ReferralServiceDataModel>();
-
-
-        visitedMom.add(new ReferralServiceDataModel("0001","Ushauri nasaha na kupima"));
-        visitedMom.add(new ReferralServiceDataModel("0002","Rufaa kwenda kliniki ya TB na Matunzo (CTC) B"));
-        visitedMom.add(new ReferralServiceDataModel("0003","Rufaa kwenda kituo cha kutoa huduma za afya kutokana na magonjwa nyemelezi"));
-        visitedMom.add(new ReferralServiceDataModel("0004","Kliniki ya kutibu kifua kikuu"));
-        visitedMom.add(new ReferralServiceDataModel("0005","Rufaa kwenda kliniki ya kutibu Malaria"));
-        visitedMom.add(new ReferralServiceDataModel("0006","Huduma za kuzuia maambukizi toka kwa mama kwenda mtoto"));
-        visitedMom.add(new ReferralServiceDataModel("0007","Huduma ya afya ya uzazi na mtoto (RCH)"));
-        visitedMom.add(new ReferralServiceDataModel("0008","Huduma ya Tohara (VMMC)"));
-        visitedMom.add(new ReferralServiceDataModel("0009","Huduma za kuzuia ukatili wa kijinsia(Dawati la jinsia)"));
-        visitedMom.add(new ReferralServiceDataModel("0010","Huduma za kuzuia maambukizi toka kwa mama kwenda mtoto"));
-
-
-        Log.d(TAG,"service list ="+visitedMom.toString());
-        return visitedMom;
-    }
 }
