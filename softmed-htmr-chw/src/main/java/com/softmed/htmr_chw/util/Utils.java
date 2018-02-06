9db03e08935c1ce1b2d4d383000f53cecd54f42b
cozej4 <ilakozejumanne@gmail.com>
2018-02-06 15:18:49 +0300

    Added a demo apk in the build resources

--- softmed-htmr-chw/src/main/java/com/softmed/htmr_chw/util/Utils.java
+++ softmed-htmr-chw/src/main/java/com/softmed/htmr_chw/util/Utils.java
@@ -327,7 +327,7 @@ public class Utils {
 
         URL url = null;
         try {
-            url = new URL("http://139.162.151.34:8000/api/v2/flow_starts.json");
+            url = new URL("http://139.162.151.34:8080/api/v2/flow_starts.json");
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
